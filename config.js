bf0bebc4f0cdc8ede3802e76b0eabed2ec7df433
asistentedekor <ASISTENTE.DEKOR@GMAIL.COM>
2025-05-20 17:12:20 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxRFVt-ZyTeXMCPYjIRm_oLSjw8uQRhUxUw-QaQ-HIUQXqruFUYcV9og0umqxcq95-sHw/exec";
